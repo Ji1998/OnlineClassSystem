ee50416797a0baeec9e722bebfe6eae1eeee66af
Steven <jiguangsen1998@126.com>
2021-01-09 18:00:25 -0500

    1.将SYSTEM 配成成SpringBoot，当跳转至/test时会显示"success"

---
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.course.system.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//如果返回的是JSON数据用RestController，如果返回的是页面用Controller
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test()
+    {
+        return "success";
+
+    }
+
+}
