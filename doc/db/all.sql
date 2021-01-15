9ab35284fb30b40e3bdb1f64851f4b8dafbae9ef
Steven <jiguangsen1998@126.com>
2021-01-14 20:21:10 -0500

    1.新增DTO层，将返回值Chapter改成ChapterDto

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -7,6 +7,9 @@ create table `chapter` (
 ) engine=innodb default charset=utf8mb4 comment='大章';
 
 
+insert into `chapter` (id, course_id, name) values ('00000000', '00000000', '测试大章一');
+insert into `chapter` (id, course_id, name) values ('00000001', '00000000', '测试大章二');
+
 # ---------------------- 测试
 
 drop table if exists `test`;
