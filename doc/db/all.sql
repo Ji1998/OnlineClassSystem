7f4181b563260d8e0aeeb2721608e2b691db1e8e
Steven <jiguangsen1998@126.com>
2021-01-23 16:01:03 -0500

    文件表设计和基本代码生成

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,4 +1,4 @@
-
+-- 课程
 drop table if exists course;
 create table course (
                         id char(8) not null default '' comment 'id',
@@ -151,6 +151,22 @@ create table `teacher` (
                            `intro` varchar(500) comment '简介',
                            primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='讲师';
+
+-- 文件
+drop table if exists `file`;
+create table `file` (
+                        `id` char(8) not null default '' comment 'id',
+                        `path` varchar(100) not null comment '相对路径',
+                        `name` varchar(100) comment '文件名',
+                        `suffix` varchar(10) comment '后缀',
+                        `size` int comment '大小|字节B',
+                        `use` char(1) comment '用途|枚举[FileUseEnum]：COURSE("C", "讲师"), TEACHER("T", "课程")',
+                        `created_at` datetime(3) comment '创建时间',
+                        `updated_at` datetime(3) comment '修改时间',
+                        primary key (`id`),
+                        unique key `path_unique` (`path`)
+) engine=innodb default charset=utf8mb4 comment='文件';
+# insert into `file` (id, path, name, suffix, size, use, created_at, updated_at) values ('1', '1', '1', '1','1','C','','');
 
 # ---------------------- 测试
 
