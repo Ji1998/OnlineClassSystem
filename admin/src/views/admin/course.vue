a70a9b4b232523aaea7f4b911fe3d77a44f618aa
Steven <jiguangsen1998@126.com>
2021-01-24 21:10:59 -0500

    分片上传测试成功

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -59,7 +59,7 @@
               <button v-on:click="toChapter(course)" class="btn btn-white btn-xs btn-info btn-round">
                 大章
               </button>&nbsp;
-              <button v-on:click="editContent(course)" class="btn btn-white btn-xs btn-info btn-round">
+              <button v-on:click="toContent(course)" class="btn btn-white btn-xs btn-info btn-round">
                 内容
               </button>&nbsp;
               <button v-on:click="openSortModal(course)" class="btn btn-white btn-xs btn-info btn-round">
@@ -97,7 +97,7 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">封面</label>
                 <div class="col-sm-10">
-                  <file v-bind:id="'image-upload'"
+                  <file v-bind:input-id="'image-upload'"
                         v-bind:text="'上传封面'"
                         v-bind:suffixs="['jpg', 'jpeg', 'png']"
                         v-bind:use="FILE_USE.COURSE.key"
@@ -182,41 +182,6 @@
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
             <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-
-    <div id="course-content-modal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">内容编辑</h4>
-          </div>
-          <div class="modal-body">
-            <form class="form-horizontal">
-              <div class="form-group">
-                <div class="col-lg-12">
-                  {{saveContentLabel}}
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-lg-12">
-                  <div id="content"></div>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-white btn-default btn-round" data-dismiss="modal">
-              <i class="ace-icon fa fa-times"></i>
-              取消
-            </button>
-            <button type="button" class="btn btn-white btn-info btn-round" v-on:click="saveContent()">
-              <i class="ace-icon fa fa-plus blue"></i>
-              保存
-            </button>
           </div>
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
@@ -405,6 +370,15 @@ export default {
       _this.$router.push("/business/chapter");
     },
 
+    /**
+     * 点击【内容】
+     */
+    toContent(course) {
+      let _this = this;
+      SessionStorage.set(SESSION_KEY_COURSE, course);
+      _this.$router.push("/business/content");
+    },
+
     allCategory() {
       let _this = this;
       Loading.show();
@@ -463,70 +437,6 @@ export default {
       })
     },
 
-    /**
-     * 打开内容编辑框
-     */
-    editContent(course) {
-      let _this = this;
-      let id = course.id;
-      _this.course = course;
-      $("#content").summernote({
-        focus: true,
-        height: 300
-      });
-
-      // 先清空历史文本
-      $("#content").summernote('code', '');
-      _this.saveContentLabel = "";
-
-      Loading.show();
-      _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course/find-content/' + id).then((response)=>{
-        Loading.hide();
-        let resp = response.data;
-
-        if (resp.success) {
-          $("#course-content-modal").modal({backdrop: 'static', keyboard: false});
-          if (resp.content) {
-            $("#content").summernote('code', resp.content.content);
-          }
-
-          // 定时自动保存
-          let saveContentInterval = setInterval(function() {
-            _this.saveContent();
-          }, 5000);
-          // 关闭内容框时，清空自动保存任务
-          $('#course-content-modal').on('hidden.bs.modal', function (e) {
-            clearInterval(saveContentInterval);
-          })
-        } else {
-          Toast.warning(resp.message);
-        }
-      });
-    },
-
-    /**
-     * 保存内容
-     */
-    saveContent () {
-      let _this = this;
-      let content = $("#content").summernote("code");
-      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/save-content', {
-        id: _this.course.id,
-        content: content
-      }).then((response)=>{
-        Loading.hide();
-        let resp = response.data;
-        if (resp.success) {
-          // Toast.success("内容保存成功");
-          // let now = Tool.dateFormat("yyyy-MM-dd hh:mm:ss");
-          let now = Tool.dateFormat("mm:ss");
-          _this.saveContentLabel = "最后保存时间：" + now;
-        } else {
-          Toast.warning(resp.message);
-        }
-      });
-    },
-
     openSortModal(course) {
       let _this = this;
       _this.sort = {
@@ -574,7 +484,8 @@ export default {
       let _this = this;
       let image = resp.content.path;
       _this.course.image = image;
-    }
+    },
+
   }
 }
 </script>
